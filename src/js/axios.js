5c5ddd6c1fe9c3183b464ca21f5374871e63a538
allen <eepson123@gmail.com>
2021-06-26 17:13:19 +0800

    add

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
-
+console.log(process.env.VUE_APP_API_URL)
 axios.interceptors.request.use(
   (confing) => {
     console.log('攔截請求')
