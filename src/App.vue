faf3f9f7176cd8e82c4cc683c5e38ab66930ac67
allen <eepson123@gmail.com>
2021-06-26 10:23:40 +0800

    OK

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="#app">
+    <div>
     <Header></Header>
     <router-view/>
     <Footer></Footer>
--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,36 @@
 module.exports = {
-  configureWebpack: {
-    // 关闭 webpack 的性能提示
-    // performance: {
-    //   hints: false
-    // },
-    // 或者
-    // 警告 webpack 的性能提示
-    performance: {
-      hints: 'warning',
-      // 入口起点的最大体积
-      maxEntrypointSize: 50000000,
-      // 生成文件的最大体积
-      maxAssetSize: 30000000,
-      // 只给出 js 文件的性能提示
-      assetFilter: function (assetFilename) {
-        return assetFilename.endsWith('.js')
-      }
-    }
-  },
-  devServer: {
-    compress: true,
-    disableHostCheck: true,
-    proxy: {
-      // 設定代理
-      '/api/*': {
-        target: 'https://vue3-course-api.hexschool.io', // 介面的域名
-        changeOrigin: true,
-        ws: true,
-        pathRewrite: { '^/api': '/api' }
-      }
-    }
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/musicstation/' : '/'
+    configureWebpack: {
+        // 关闭 webpack 的性能提示
+        // performance: {
+        //   hints: false
+        // },
+        // 或者
+        // 警告 webpack 的性能提示
+        performance: {
+            hints: 'warning',
+            // 入口起点的最大体积
+            maxEntrypointSize: 50000000,
+            // 生成文件的最大体积
+            maxAssetSize: 30000000,
+            // 只给出 js 文件的性能提示
+            assetFilter: function(assetFilename) {
+                return assetFilename.endsWith('.js')
+            }
+        }
+    },
+    devServer: {
+        compress: true,
+        disableHostCheck: true,
+        proxy: {
+            // 設定代理
+            '/api/*': {
+                target: 'https://vue3-course-api.hexschool.io', // 介面的域名
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: { '^/api': '/api' }
+            }
+        }
+    },
+    publicPath: process.env.NODE_ENV === 'dev' ? '/musicstation/' : '/'
 
 }
