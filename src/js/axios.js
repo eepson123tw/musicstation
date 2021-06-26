75189ff43036d7ce836d1a00cc4b958451548062
allen <eepson123@gmail.com>
2021-06-26 17:37:14 +0800

    fix gitpage

--- src/js/axios.js
+++ src/js/axios.js
@@ -5,7 +5,7 @@ axios.defaults.baseURL = 'https://vue3-course-api.hexschool.io'
 axios.interceptors.request.use(
   (confing) => {
     console.log('攔截請求')
-    console.log(process.env.VUE_APP_API_URL)
+    console.log('process', process.env.VUE_APP_API_URL)
     return confing
   },
   (error) => {
